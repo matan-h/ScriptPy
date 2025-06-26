4629319cb696452e03759f75817ece66b9e3b0c2
matan h <matan.honig2@gmail.com>
2025-06-26 15:12:35 +0300

    add verbose

--- scriptpy/main.py
+++ scriptpy/main.py
@@ -3,7 +3,6 @@ import ast
 import io
 import linecache
 import tokenize
-from typing import Type
 
 import astor
 
@@ -15,7 +14,7 @@ from .smart_eval import balance_fix, smart_parse, smart_run
 
 
 
-def custom_eval(src: str, globals_: dict | None = None):
+def custom_eval(src: str, globals_: dict | None = None,verbose=False):
     # ——— 1) token-level rewrite of “|.name…” → “| _apipe('name',…)”
     src = balance_fix(src)
 
@@ -47,7 +46,8 @@ def custom_eval(src: str, globals_: dict | None = None):
         tree = transformer().visit(tree)
 
     ast.fix_missing_locations(tree)
-    print(f"[DEBUG] Transformed code:```\n{astor.to_source(tree).strip()}\n```\n")
+    if verbose:
+        print(f"[DEBUG] Transformed code:```\n{astor.to_source(tree).strip()}\n```\n")
     # code = compile(tree, filename, 'eval')
 
     # ——— 3) eval with our small helpers in scope
@@ -69,15 +69,17 @@ def main():
     group.add_argument(
         "-c", "--code", type=str, help="scriptpy code provided as a string"
     )
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
 
     args = parser.parse_args()
 
     if args.filename:
         with open(args.filename, "r") as f:
             code = f.read()
-        print(custom_eval(code))
-    elif args.code:
-        print(custom_eval(args.code))
+    else:
+        code = args.code
+
+    print(custom_eval(code, verbose=args.verbose))
 
 
 if __name__ == "__main__":
